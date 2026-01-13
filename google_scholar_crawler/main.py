34fb4de58c9c90653a3b97467647b79079dfc759
SCNU_WQF <65220662+Jorffy@users.noreply.github.com>
2026-01-13 16:02:48 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -12,7 +12,8 @@ try:
     pg = ProxyGenerator()
     pg.FreeProxies()  # Use free rotating proxies
     scholarly.use_proxy(pg)
-    author: dict = scholarly.search_author_id(os.environ["GOOGLE_SCHOLAR_ID"])
+    # author: dict = scholarly.search_author_id(os.environ["GOOGLE_SCHOLAR_ID"])
+    author: dict = scholarly.search_author_id("zo8SfrMAAAAJ")
 except MaxTriesExceededException as e:
     print(f"发生异常: {e}")
 else:
